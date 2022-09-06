9068724da760b92cac2f2ede6bacded85541fec1
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2022-09-06 10:09:45 +0100

    log sample http server

---
+++ internal/server/log.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"fmt"
+	"sync"
+)
+
+var ErrOffsetNotFound = fmt.Errorf("offset not found")
+
+type Record struct {
+	Value  []byte `json:"value"`
+	Offset uint64 `json:"offset"`
+}
+
+type Log struct {
+	mu      sync.RWMutex
+	records []Record
+}
+
+func NewLog() *Log {
+	return &Log{}
+}
+
+func (c *Log) Append(record Record) (uint64, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	record.Offset = uint64(len(c.records))
+	c.records = append(c.records, record)
+	return record.Offset, nil
+}
+
+func (c *Log) Read(offset uint64) (Record, error) {
+	c.mu.RLock()
+	defer c.mu.Unlock()
+	if offset >= uint64(len(c.records)) {
+		return Record{}, ErrOffsetNotFound
+	}
+	return c.records[offset], nil
+}
